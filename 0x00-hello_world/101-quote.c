97c3f051a9d5279705d3797d4b4b8c34cc2f23b0
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-10 15:51:11 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 /**
- *main - Entry point
- *
- * Description: 'first time printing without using main functions'
- *
- * Return: Always 1 (Success)
- */
+  * main - Entry point
+  * Return: Always 1 (Success)
+  */
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
