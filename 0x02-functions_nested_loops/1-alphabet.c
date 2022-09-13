44bc82e8b1e3f43723ccf30f4b8217f10703100b
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-13 16:18:27 -0700

    Everything has a function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
   * print_alphabet - Check description
   * Description: It prints the alphabet in lowercase fallowed by a new line
@@ -10,6 +10,6 @@ void print_alphabet(void)
 		char i;
 
 		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
-		_putchar('\n');
+			putchar(i);
+		putchar('\n');
 }
