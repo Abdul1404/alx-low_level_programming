d3f657e8fdf992712697c0c4e430e9939532cf2e
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-15 15:18:14 -0700

    &:

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,10 @@
 
 void print_number(int n)
 {
-	unsigned int tens, digit, positive = n;
-	double t_beg = 1;
+	int n = '0';
 
-	if (n == 0)
-	_putchar('0');
-	else
-	{
-	if (n < 0)
-	{
-	positive = n * -1;
-	_putchar('-');
-	}
+	for (; n <= '1000'; n++)
+	_putchar(n);
 
-	while (t_beg <= positive)
-	t_beg *= 10;
-	tens = t_beg / 10;
-
-	while (tens >= 1)
-	{
-	digit = positive / tens;
-	_putchar(digit + '0');
-	positive = (positive - (tens * digit));
-	tens /= 10;
-	}
-	}
+	_putchar('\n');
 }
