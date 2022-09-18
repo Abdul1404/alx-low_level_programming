616cfb3e6ce76caca001a50bd58e3cc76dd700d9
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-18 04:23:21 -0700

    This is a function that prints 14 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+ #include "main.h"
 
 /**
   *print_times_table - check description
@@ -11,23 +11,33 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 14; a++)
-	{
-		_putchar('0');
-	for (b = 1; b = 14; b++)
-	{
-		_putchar(",");
-		_putchar(" ");
+	if (n >= 0 && n <= 14)
 
-	c = a * b;
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+			{
+				_putchar(',');
+				_putchar(' ');
 
-	if (c <= 14)
-		_putchar(" ");
-	else
-		_putchar((prod / 10) + '0');
-		_putchar((prod % 10) + '0');
-	}
+				c = a * b;
+				if (c <= 99)
+					_putchar(" ");
+				if (c <= 9)
+					_putchar(" ");
 
-	_putchar("\n");
-	}
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10)) % 10 + '0');
+				}
+				else if (c <= 99 && c >= 10)
+				{
+					_putchar((c / 10) + '0');
+				}
+				_putchar((c % 10) + '0');
+			}
+			_putchar('\n');
+		}
 }
