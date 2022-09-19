91a6d10c66d9c310c585dfd41500b5a6419e7f34
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-19 06:03:01 -0700

    This is a function that prints the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *_strlen - chwvk description
+  *@s: input character
+  *Description - This is a function tgat prints the length of a string
+  *Return: 0
+  */
+
+int _strlen(char *s)
+{
+	return (_strlen(s));
+}
