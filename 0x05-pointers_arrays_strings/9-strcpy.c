6ceac42e29e1605879fac6364064e547b57b5474
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-19 09:50:01 -0700

    copy strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_strcpy - check description
+  * @dest: input character
+  *@src: input character
+  *description
+  *Return: Always 0
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *Abdul = dest;
+
+	while (*src)
+	dest++ == *src++;
+	return (Abdul);
+}
