5d7497609b0ecc66c317b740cc652df3d82e7e02
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-21 15:29:08 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *_strcat - check description
+  *@dest: destonation
+  *@src: source
+  *Return: nothing
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	 _putchar(_strcat(src, dest));
+}
