2abe417532f9159e7aeebabe93f3ab56d5cec458
Abdul1404 <abdulsalamabdulsomad14@gmail.com>
2022-09-28 09:42:05 -0700

    correcte dindentation

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-  *_pow_recursion - rases x to tge power of x
-  *@x: variable to raise
-  *@y: variable to be raised to
-  *Return: p
-  */
+* _pow_recursion - return the value of `x` raised to power of `y`
+* @x: number value
+* @y: number power
+* Return: x to power of y
+ */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (-1);
+	return (-1);
 	else if (y == 0)
 		return (1);
 	else if (y == 1)
