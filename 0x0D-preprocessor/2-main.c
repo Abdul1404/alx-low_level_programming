79d63befaf21f0abd268e27e41c7cc66fb7b37a1
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-10-10 10:00:59 -0700

    Worte a program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+/**
+  *main - Entry point
+  *Description - a function that prints the name of the
+  *	file it was compiled from, followed by a new line
+  *
+  *Return: 0
+  */
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
