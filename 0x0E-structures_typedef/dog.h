aad4888354d39dbb59af4ac6e1b02a2d5dec9433
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-10-11 03:30:53 -0700

    Defined a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _dog_h
+#define _dog_h
+
+/**
+  *struct dog - Defines a new type struct dog with
+  *			the following elements
+  *@name: first memeber
+  *@owner: second member
+  *@age: Third memeber
+  */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /*_dog_h*/
