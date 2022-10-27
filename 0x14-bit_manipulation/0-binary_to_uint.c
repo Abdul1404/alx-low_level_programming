cc0143e79be53b54f6d92787d0a297bc8424454f
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-10-27 07:12:33 -0700

    returns 0 if b is NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 	if (b == NULL)
-		return (NULL);
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
