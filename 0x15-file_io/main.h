297489e3a90228615d849de906a3e1fb011a6b7e
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-10-31 05:58:46 -0700

    Wrote a program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,16 @@
 #include <sys/stat.h>
 #include <stdarg.h>
 #define BUFFSIZE 1024
+#include <elf.h>
 
+void get_entry(Elf64_Ehdr *ehdr);
+void get_type(Elf64_Ehdr *ehdr);
+void get_vabi(Elf64_Ehdr *ehdr);
+void get_osabi(Elf64_Ehdr *ehdr);
+void get_velf(Elf64_Ehdr *ehdr);
+void get_data(Elf64_Ehdr *ehdr);
+void get_class(Elf64_Ehdr *ehdr);
+void get_magic(Elf64_Ehdr *ehdr, char *fname, int fd);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
